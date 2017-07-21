a74e370ab5a53c9dc15e092c6fa91a848227bf9b
wendyliu8 <wenhongliu@Wenhongs-MBP.home>
2017-07-21 14:46:01 -0700

    part 3 results

--- client/client.js
+++ client/client.js
@@ -1,1 +1,30 @@
+/*
+ 518  npl install -g webpack
+  519  npm install -g webpack
+  520  which webpack
+  521  webpack
+  522  webpack --config webpack.config.js 
+  523  nodemon server/server.js
+  524  webpack --config webpack.config.js 
+  525  webpack --config webpack.config.js 
+  526  nodemon server/server.js
+  527  webpack --config webpack.config.js 
+  528  nodemon server/server.js
+  529  webpack --config webpack.config.js 
+  530  webpack --config webpack.config.js 
+  531  nodemon server/server.js
 
+
+*/
+
+
+import React from 'react'
+import { render } from 'react-dom'
+import App from '../components/App'
+
+render(
+	//define the encompassng component,
+	// DOM element we want to moun tit to
+	<App/>,
+	document.getElementById('app')
+)
--- components/App.js
+++ components/App.js
@@ -1,1 +1,11 @@
+import React from 'react'
 
+var App = React.createClass( {
+
+	render() {
+		return <div>This is a React app now!</div>	
+	}
+
+})
+
+export default App // allows to import in client.js
